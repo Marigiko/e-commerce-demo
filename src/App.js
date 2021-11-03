781715645fa13fbd53d1c37611730c4b101bde0e
Marigiko <mark221@hotmail.es>
2021-11-02 22:10:17 -0300

    refactorizado

--- src/App.js
+++ src/App.js
@@ -3,21 +3,45 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div class="grid-container">
+      <header class="header">
+        <div class="brand">
+          <button onclick="openMenu()">
+            &#9776;
+          </button>
+          <a href="index.html">E-commerce</a>
+        </div>
+        <div class="header-links">
+          <a href="cart.html">Cart</a>
+          <a href="signin.html">Sign in</a>
+        </div>
       </header>
+      <aside class="sidebar" >
+        <h3>Shopping Categories</h3>
+        <button class="sidebar-close-button" onclick="closeMenu()" >x</button>
+        <ul>
+          <li><a href="index.html" >Pants</a></li>
+          <li><a href="index.html" >Shirts</a></li>
+        </ul>
+      </aside>
+      <main class="main">
+        <div class="content">
+          <ul class="products">
+            <li>
+              <div class="product">
+                <img class="product-image" src="./image/d1.jpg" alt="product" />
+                <div class="product-name">
+                  <a href="product.html">Slim Shirt</a>
+                </div>
+                <div class="product-brand">Nike</div>
+                <div class="product-price">$60</div>
+                <div class="product-rationg">4.5 Stars (10 Reviews)</div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </main>
+      <footer class="footer">All rights reserved.</footer>
     </div>
   );
 }
